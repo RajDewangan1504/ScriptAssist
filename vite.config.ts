022beb892f0d1e40728ee768704c75bc9ba71e31
Rajkumar Dewangan <rajdewangan020@gmail.com>
2025-02-14 00:26:02 +0530

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/", // Ensure base path is set correctly for Vercel
-  server: {
-    host: "0.0.0.0",  // Allow access from Render
-    port:  5175, // Use Render's assigned port
-    strictPort: false
-  },
-  
-  build: {
-    outDir: "dist"
-  },
-  resolve: {
-    alias: {
-      "@": "/src"
-    }
-  }
+	server: {
+		port: 5175
+	},
+	plugins: [react()],
+	build: {
+		outDir: 'build'
+	}
 });
+
